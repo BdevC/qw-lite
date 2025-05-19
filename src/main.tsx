390fc1c6c95504b1463a8056f160b5246e804cfe
Ben <benc6053@gmail.com>
2025-05-19 15:13:07 -0400

    minimize app start

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import Head from './Head.tsx'
 import App from './App.tsx'
+import Foot from './Foot.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Head />
     <App />
+    <Foot />
   </StrictMode>,
 )
